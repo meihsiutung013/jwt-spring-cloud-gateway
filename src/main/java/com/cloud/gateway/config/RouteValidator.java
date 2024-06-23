1699e7a2afe524f9f3e73837b9f76c025c9009ed
Mei Tung <meihsiu.tung013@comunidadunir.net>
2024-06-23 04:43:28 -0500

    update user endpoint

--- src/main/java/com/cloud/gateway/config/RouteValidator.java
+++ src/main/java/com/cloud/gateway/config/RouteValidator.java
@@ -32,7 +32,8 @@ public class RouteValidator {
     public static final List<Endpoint> authEndpoints = List.of(
             // microservice-users
             new Endpoint("/api/users", "GET", "USER"),
-            new Endpoint("/api/users", "PUT", "USER")
+            new Endpoint("/api/users", "PUT", "USER"),
+            new Endpoint("/api/users", "DELETE", "USER")
     );
 
     /*
